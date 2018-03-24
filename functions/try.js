de88c53d7c33dd08efe20a89019834d4dab5edd2
kaivalya819 <34744886+kaivalya819@users.noreply.github.com>
2018-03-24 20:21:14 +0530

    Update try.js

--- functions/try.js
+++ functions/try.js
@@ -1,151 +1,186 @@
-
-
-
-
-yet
-done
-noresult
-
-
-
-
-
-
-
-
-
 const user = require('../models/user');
 const mat=user.mat;
 const minfo=user.matchinf;
 const usersc =user.user;
+const { forEach } = require('p-iteration');
+const {async} = require('async');
 
-var mcount;
+var results=[];
 
 
 
-exports.getIndividual=(matchId,phone)=>
+
+
+async function getUpdated(doc){
+	return new Promise((resolve,reject)=>{
+		console.log("are we here yet");
+		 usersc.findOne({phone:doc.phone}).then(got=>{
+			    var bs1=got.matchPersonal.m60.bs1;
+				var bs2=got.matchPersonal.m60.bs2;
+			    var bs3=got.matchPersonal.m60.bs3;
+			    var bs4=got.matchPersonal.m60.bs4;
+			    var bs5=got.matchPersonal.m60.bs5;
+			    var bs6=got.matchPersonal.m60.bs6;
+			    var bs7=got.matchPersonal.m60.bs7;
+			   
+                 var calc=got.matchPersonal.m60.ifcalc;
+			   
+				var bo1=got.matchPersonal.m60.bo1;
+				var bo2=got.matchPersonal.m60.bo2;
+			    var bo3=got.matchPersonal.m60.bo3;
+			    var bo4=got.matchPersonal.m60.bo4;
+			    var bo5=got.matchPersonal.m60.bo5;
+			    var bo6=got.matchPersonal.m60.bo6;
+			    var bo7=got.matchPersonal.m60.bo7;
+                
+				var points=got.points;
+				var rpoints=got.rpoints;
+			 var pm1=bo1+bo2+bo3+bo4+bo5+bo6+bo7;
+
+				
+				if(bs1==results[0])
+				{
+					if(bs1==results[9])
+					{
+						bo1=(bo1*results[7]);
+						
+					}else
+						bo1=(bo1*results[8]);
+					
+					
+				}else{bo1=(-bo1);}
+				
+				
+				if(bs2==results[1]){bo2=(bo2*3);}else{bo2=(-bo2);}
+				
+				if(bs3==results[2]){bo3=(bo3*3);}else{bo3=(-bo3);}
+				
+				if(bs4==results[3]){bo4=(bo4*3);}else{bo4=(-bo4);}
+				
+				if(bs5==results[4]){bo5=(bo5*3);}else{bo5=(-bo5);}
+
+				if(bs6==results[5]){bo6=(bo6*5);}else{bo6=(-bo6);}
+				
+				if(bs7==results[6]){bo7=(bo7*3);}else{bo7=(-bo7);}
+
+				var fpoints=bo1+bo2+bo3+bo4+bo5+bo6+bo7+rpoints+pm1;
+				var pm=bo1+bo2+bo3+bo4+bo5+bo6+bo7+points;
+				var pmsts=bo1+bo2+bo3+bo4+bo5+bo6+bo7;
+                
+			if(calc==0)	{
+				
+		       usersc.updateOne({phone:doc.phone},{$set:{"matchPersonal.m60.bo1":bo1,"matchPersonal.m60.bo2":bo2,
+			  "matchPersonal.m60.bo3":bo3,"matchPersonal.m60.bo4":bo4,"matchPersonal.m60.bo5":bo5,"matchPersonal.m60.bo6":bo6,"matchPersonal.m60.bo7":bo7,"rpoints":fpoints,"points":pm,"matchPersonal.m60.ifcalc":1,"matchPersonal.m60.pmstatus":pmsts
+			  
+			  
+			  }}).then(result=>{
+                              
+					console.log("result",result);
+                    resolve({result});        
+
+
+					
+				  
+			  }
+			  
+			               
+					
+					
+			).catch(err=>reject({status:500}));}
+					else{
+						resolve({});
+						
+					}
+			
+		}
+		     
+		
+		).catch(err=>reject({status:500,message:"Internal server Error!"}));
+		
+		
+	});
+	
+}
+
+
+async function anotFunc(){
+	
+	var docs=await usersc.find({});
+	for(let doc of docs){
+		
+		await getUpdated(doc);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+exports.getSorted=()=>
+
 new Promise((resolve,reject)=>{
-	var e="matchPersonal.m"+1+".bo1";
+	afunc().then(ge=>{
+		
+		anotFunc().then(ree=>{
+		
+		resolve({status:200});}).catch(err=>reject({status:500,message:"Internal server Error!"}));
+		
+	}
+	
+	).catch(err=>reject({status:500,message:"Internal server Error!"}));
+	
+	
+	
+}
 
-	query={};
-	query[e]=1;
-var mNum="m"+1;
-var we="bo"+1;
-var quer=usersc.findOne({phone:phone}).select(query);
-quer.exec(function (err, matc) {
-
-var k=matc.toObject();
-var mNum="m"+1;
-var we="bo"+1;
-var b=k.matchPersonal[mNum][we];
-console.log('',b);
-
-
-
-resolve({status:200,k});
-if(err){reject({status:500,message:'Internal Server Error'});}
-});
-});
-
-exports.getUserPoints=(phone,m,c)=>
-new Promise((resolve,reject)=>{
-	usersc.findOne({phone:phone}).then(guser=>{
-		var points=guser.points;
-		var g=""+m;
-
-		var HpEleven=[];
-		var VpEleven=[];
-		var teams=[];
-		mat.findOne({_id:"2"}).then(grab=>{
-			teams.push(grab.hTeam);
-
-			teams.push(grab.vTeam);
-			console.log("",teams);
-			minfo.findOne({_id:grab.hTeam}).then(gr=>{
-
-				for(var t in gr.team){
-					HpEleven.push(gr.team[0]);
-
-				}
-
-			});
-			minfo.findOne({_id:grab.vTeam}).then(gr=>{
-				var r=gr.toObject();
-				console.log("",r.team);
-				for(var e in gr.team){
-					VpEleven.push(gr.team[e]);
-				}
-
-			});
-		});
-
-		console.log("",points);
-		console.log("",teams);
-		console.log("",HpEleven);
-		console.log("",VpEleven);
+);
 
 
-		resolve({status:200,points,teams,HpEleven,VpEleven});
+async function afunc(){
+	return new Promise((resolve,reject)=>{
+		
 
-	}).catch(err => reject({ status: 500, message: 'Internal Server Error !' }));
+		mat.findOne({_id:"60"}).then(grab=>{
+				
+				console.log("here as well2",grab);
+			var d=grab.mResult.mWinner;
+			
+							console.log("here as well4",d);
+			results[0]=grab.mResult.mWinner;
 
+			results[1]=grab.mResult.mRuns1;
+			results[2]=grab.mResult.mWickets1;
+				console.log("here as well2",grab);
 
-});
-
-
-
-
-exports.getmatchStatusData=()=>
-
-   new Promise((resolve,reject)=>{
-
-	   mat.find({})
-
-	   .then(grab=>{
-		            var sDates=[];
-					var eDates=[];
-              		var statusArray=[];
-for(var i=1;i<=55;i++){
-console.log("inside status");
-				var d=new Date();
-				sDates.push(grab[i].sDate);
-				eDates.push(grab[i].eDate);
-				console.log("before",d,grab[i].eDate);
-				if(d.getTime()>=grab[i].sDate.getTime() && d.getTime()<=grab[i].eDate.getTime()){
-				console.log("after");
-				statusArray.push("open")}else if(d.getTime()>grab[i].eDate.getTime()){
-	   statusArray.push("closed");}
-	   else
-		   statusArray.push("not available");
-
-	   }
-
-
-		   console.log('',statusArray);
-
-
-		resolve({status:200,r,statusArray,sDates,eDates});
-	   }
-
-	   )
-
-
-
-
-.catch(err => reject({ status: 500, message: 'Internal Server Error !' }));
-   });
-
-
-	function getFirstDetails(u){
-		console.log("working");
-	 return new Promise((resolve,reject)=>{
-
-	 usersc.find({phone:"4545454545"},{matchPersonal:0}).then(grab =>{
-		              console.log("found");
-		 var pnts="400";
-		 resolve({pnts});
-	 }).catch(err => reject({status: 500, message: 'Internal server error !'}));
-
-
-
- });}
+			results[3]=grab.mResult.mRuns2;
+			results[4]=grab.mResult.mWickets2;
+			
+			results[5]=grab.mResult.manMatch;
+			results[6]=grab.mResult.tsixes;
+			
+			results[7]=parseInt(grab.Ratio.rHteam);
+			results[8]=parseInt(grab.Ratio.rVteam);
+			
+			results[9]=grab.hTeam;
+			results[10]=grab.vTeam;
+			
+				console.log("here as well 3",results);
+			
+			resolve({results});
+		}
+	                   	
+		
+		).catch(err=>reject({status:500,message:"Internal server Error!"}));
+	        	
+		
+	}
+	
+	);
+	
+	
+}
